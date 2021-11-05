f0b6b7f58f87ffb194144601c352c14e8055e4f1
soft7net <sof7net@gmail.com>
2021-11-05 05:02:00 +0100

    clean up

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,23 @@
-<script setup lang="ts">
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
-import HelloWorld from './components/HelloWorld.vue'
-</script>
 
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
+  Hello {{word}}
 </template>
 
-<style>
+<script setup lang="ts">
+
+  import { ref } from 'vue'
+
+  const word = ref('world')
+
+</script>
+
+<style lang="scss">
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: Roboto, Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
+
 </style>
